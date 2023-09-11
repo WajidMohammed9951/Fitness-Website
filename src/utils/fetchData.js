bf52d6750e5128cfdd4d0346e2677617df50ed92
Mohammed Wajid <wajid17023@gmail.com>
2023-09-12 02:19:19 +0530

    installing run build

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '9993b5c451msh84f51b51c1599bdp1ce851jsn8c8542f2c12a',
   },
 };
 
